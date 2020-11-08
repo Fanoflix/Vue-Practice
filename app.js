5995409b8f46bc272fbda2941c9d1a6d9a45d32a
Ammar Bin Nasir <majidammar428@gmail.com>
2020-11-08 21:57:26 +0500

    v-on:click
    --- v-on:click ----
    
    - Adds an event listener
    
    - Can also be written as "@click"

--- app.js
+++ app.js
@@ -1,24 +1,19 @@
 const app = Vue.createApp({
     data() {
         return {
-            courseGoalA: "Finish the course!",
-            courseGoalB: "<h2>Master vue by completing this course!</h2>",
-            vueLink: 'https://vuejs.org/'
+            counter: 0,
         };
     },
     methods: {
-        outputGoal() { // This function can be called normally in the {{ }} brackets inside our HTML.
-                        // this in vue is magic. It takes all the data you return in this data() object
-                        // and it merges it into a global vue instance object (Into your vue app object)
-                        // Same with methods, they are also available.
-            const randomNumber = Math.random();
-            if (randomNumber < 0.5) {
-                return this.courseGoalA;
-            } else {
-                return this.courseGoalB;
+        IncrementCounter() {
+            this.counter++;
+        },
+        DecrementCounter() {
+            if (this.counter > 0) {
+                this.counter--;
             }
         }
     }
-});
+})
 
-app.mount('#user-goal');
+app.mount('#events');
