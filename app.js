66cc8cfaf00fe61e0db1880a41534ac81ce21cb3
Ammar Bin Nasir <majidammar428@gmail.com>
2020-11-09 15:03:30 +0500

    $event Reserved Name
    --- SetName($event, 'Nasir') ---
    
    - Use this in the HTML code to pass another argument
    
    - $event is a reserved word that contains the information about the element the event occured on.
    
    - I don't reckoen you'd want to use this very often.

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ const app = Vue.createApp({
                 this.counter = this.counter - num;
             }
         },
-        SetName(event) {
-            this.name = event.target.value; // This is the value that the user entered.
+        SetName(event, lastName) {
+            this.name = event.target.value + ' ' + lastName; // This is the value that the user entered.
                                             // Now we cannot use the "Calling method" in our HTML.
                                             // Must use "Pointing method".
         } // event.target gives us access to the element on which the event occured.
