3d50eb72dd24ef1c41950f18d7aad606789a6a6c
Ammar Bin Nasir <majidammar428@gmail.com>
2020-11-09 13:58:33 +0500

    Native Event Object
    --- event Object ---
    
    - Within an input tag, you can  specificy a v-on:input binding, that will execute whataver is in the inverted commas on EVERY key stroke.
    
    - In this case, the method SetName will be called on every keystroke inside the input field. (Note that we can only use the "Pointing method" here for everything to function properly)
    
    - Now in our js file, we have to get access to whatever the user enters.
    
    - We take in a parameter to the function SetName(event).
    
    - Now we can acecess the element on which the event occured by using "event.target"
    
    - Now we can extract the value that the user entered by using event.target.value

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const app = Vue.createApp({
     data() {
         return {
             counter: 0,
+            name: '',
         };
     },
     methods: {
@@ -12,7 +13,12 @@ const app = Vue.createApp({
             if (this.counter > 0) {
                 this.counter = this.counter - num;
             }
-        }
+        },
+        SetName(event) {
+            this.name = event.target.value; // This is the value that the user entered.
+                                            // Now we cannot use the "Calling method" in our HTML.
+                                            // Must use "Pointing method".
+        } // event.target gives us access to the element on which the event occured.
     }
 })
 
