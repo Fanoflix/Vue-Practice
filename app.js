d4054fa4dfcd905deb26120f0048e03003e79192
Ammar Bin Nasir <majidammar428@gmail.com>
2020-11-30 23:22:17 +0500

    Watchers vs Computer
    - Heavy info in comments

--- app.js
+++ app.js
@@ -3,42 +3,54 @@ const app = Vue.createApp({
         return {
             counter: 10,
             name: '',
-            fullName: '',
+            // fullName: '',
             lastName: '',
         };
     },
     watch: { // We can use the name we used in the data or computer property as a name for the watcher method
-        name(value) { // This tells Vue, that whenever the data attribute "name" changes, this watcher method will re-execute.
-        // We DO NOT return anything here
-        // The latest value of the watched property is automatically passed into the watched property.
-            if (value === '') {
-                this.fullName = '';
-            }
-            else {
-                this.fullName = value + ' ' + this.lastName;
-            }
-        },
-        lastName(value) {
-            if (value === '') {
-                this.fullName = '';
-            }
-            else {
-                this.fullName = this.name + ' ' + value;
-            }
-        }
-
+        // name(value) { // This tells Vue, that whenever the data attribute "name" changes, this watcher method will re-execute.
+        // // We DO NOT return anything here
+        // // The latest value of the watched property is automatically passed into the watched property.
+        //     if (value === '') {
+        //         this.fullName = '';
+        //     }
+        //     else {
+        //         this.fullName = value + ' ' + this.lastName;
+        //     }
+        // },
+        // lastName(value) {
+        //     if (value === '') {
+        //         this.fullName = '';
+        //     }
+        //     else {
+        //         this.fullName = this.name + ' ' + value;
+        //     }
+        // },
+        // counter(value) {
+        //     if (value > 50) {
+        //         this.counter = 0;
+        //     }
+        // },
     },
     computed: {
-        // fullName() { // Naming this as a data property.
-        //             // You should name your computed properties just like your data properties.
-        //     console.log("Running COMPTUED again!....")
-        //     if (this.name === '') {
-        //         return '';
+        // Using the fullName() computer property is a better choice than using 2 watchers (one for name, and the other for lastName) because its a lot of code. Computed is the better choice here.
+
+        // This resetCounter() computer property will have to be CALLED to work. The watcher above (line 29) named 'counter' is basically watching the variable counter for any changes within in. Therefore it doesnt need to be called. Watcher is the better choice here.
+
+        // resetCounter() { 
+        //     if (counter > 50) {
+        //         counter = 50;
         //     }
-        //     if (this.name.length > 4){ 
-        //         return this.name +' ' + 'Nasir';
-        //     }
-        // }
+        // },
+        fullName() { // Naming this as a data property.
+                    // You should name your computed properties just like your data properties.
+            if (this.name === '' || this.lastName === '') {
+                return '';
+            }
+            if (this.name.length > 4){ 
+                return this.name +' ' + this.lastName;
+            }
+        }
     },
     methods: {
         ResetInput() {
