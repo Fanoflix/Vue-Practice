c3482a377617cc42f516cd333970fbc0e137627b
Ammar Bin Nasir <majidammar428@gmail.com>
2020-11-30 22:15:30 +0500

    --- Computed Properties ---
    - methods are not the best solution for outputting dynamically calculated values (like the OutputFullName method)
    
    For this purpse, we use Computed properties.
    
    ---
    - First was the data, Second was the method, third is the computed property.
    
    - You should name your computed properties just like your data properties.
    
    - Don't use paranthese for computed properties in your HTML code. We don't CALL them, we just POINT to them and Vue calls them for us.
    
    - We use it just like we use a Data property
    
    - Vue is aware of the dependencies of the computer property. Unlike in methods.
    
    - You in reality only use comptuer properties for outputting something.
    
    - You don't bind your events to computer properties.

--- app.js
+++ app.js
@@ -3,16 +3,24 @@ const app = Vue.createApp({
         return {
             counter: 10,
             name: '',
-            confirmedName: '',
+            // confirmedName: '',
         };
     },
+    computed: {
+        fullName() { // Naming this as a data property.
+                    // You should name your computed properties just like your data properties.
+            console.log("Running COMPTUED again!....")
+            if (this.name === '') {
+                return '';
+            }
+            if (this.name.length > 4){ 
+                return this.name +' ' + 'Nasir';
+            }
+        }
+    },
     methods: {
-        ConfirmInput() {
-            this.confirmedName = this.name;
-        },
         ResetInput() {
             this.name = '';
-            this.confirmedName= '';
             this.counter = 0;
         },
         SubmitForm() {
@@ -26,11 +34,6 @@ const app = Vue.createApp({
                 this.counter = this.counter - num;
             }
         },
-        // SetName(event, lastName) {
-        //     this.name = event.target.value; // This is the value that the user entered.
-        //                                     // Now we cannot use the "Calling method" in our HTML.
-        //                                     // Must use "Pointing method".
-        // } // event.target gives us access to the element on which the event occured.
     }
 })
 
