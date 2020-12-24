42d4ff0db9ce6ce0871ffbaf3fa5b1c523f4ec1c
Ammar Bin Nasir <majidammar428@gmail.com>
2020-12-24 13:23:06 +0500

    Dynamic Styling with In-line styles
     - NOTE: restted app.js and index.html -
    
    -
    
    - For BINDING styles and for classes attributes of HTML, vue has a special syntax
    
    - Once you've bound the styles, you can feed it an object now (which normally isnt possible
    
    - We can now use CSS property name inside it. Its RECOMMENDED to use camel case to reference CSS propoerties in here. As dashes are not allowed in javascript objects.
    
    So border-color should be written as borderColor
    
    Syntax: :style="{borderColor: 'red'}"
    
    But we didnt bind the style attribute to just change the syntax.
    
    - We can now change the colors dynamically.
    
    Syntax: :style="{borderColor: boxAselected ? 'red' : '#ccc'}"
    
    NOTES:
    
    - Its not recommended to bind styles, because they override EVERY other styling, which we do not wan most of the time.
    
    More on this in the next commit.

--- app.js
+++ app.js
@@ -1,46 +1,28 @@
-const app = Vue.createApp({ 
+const app = Vue.createApp({
     data() {
         return {
-            counter: 10,
-            name: '',
-            lastName: '',
+            boxAselected: false,
+            boxBselected: false,
+            boxCselected: false
         };
     },
-    watch: {
-        counter(value) {
-            if (value > 50) {
-                this.counter = 0;
-            }
-        },
-    },
-    computed: {
-        fullName() { // Naming this as a data property.
-                    // You should name your computed properties just like your data properties.
-            if (this.name === '' || this.lastName === '') {
-                return '';
-            }
-            if (this.name.length > 4){ 
-                return this.name +' ' + this.lastName;
-            }
-        }
-    },
     methods: {
-        ResetInput() {
-            this.name = '';
-            this.counter = 0;
-        },
-        SubmitForm() {
-            alert('Submitted!');
-        },
-        IncrementCounter(num) {
-            return this.counter = this.counter + num;
-        },
-        DecrementCounter(num) {
-            if (this.counter > 0) {
-                this.counter = this.counter - num;
+        boxSelected(boxx) {
+            if (boxx === 'A')  {
+                if (this.boxAselected == false) { this.boxAselected = true;}
+                else if (this.boxAselected == true) { this.boxAselected = false }
+            }
+            else if (boxx === 'B')  {
+                if (this.boxBselected == false) { this.boxBselected = true;}
+                else if (this.boxBselected == true) { this.boxBselected = false }
+            }
+            else if (boxx === 'C')  {
+                if (this.boxCselected == false) { this.boxCselected = true;}
+                else if (this.boxCselected == true) { this.boxCselected = false }
             }
         },
-    }
-})
+    },
 
-app.mount('#events');
+});
+
+app.mount('#styling')
