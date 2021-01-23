593f7c6a507cc6c126b4f1b439797c8b75550142
Ammar Bin Nasir <majidammar428@gmail.com>
2021-01-23 19:12:31 +0500

    Rendering Conditional Content and Lists
    -- Initiated Starting Setup (HTML, CSS, JS files)

--- app.js
+++ app.js
@@ -1,31 +1,7 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            boxAselected: false,
-            boxBselected: false,
-            boxCselected: false
-        };
-    },
-    computed: {
-        boxAClasses() {
-            return {active: this.boxAselected};
-        }
-
-    },
-    methods: {
-        boxSelected(boxx) {
-            if (boxx === 'A')  {
-                this.boxAselected = !this.boxAselected;
-            }
-            else if (boxx === 'B')  {
-                this.boxBselected = !this.boxBselected;
-            }
-            else if (boxx === 'C')  {
-                this.boxCselected = !this.boxCselected;
-            }
-        },
-    },
-
+  data() {
+    return { goals: [] };
+  },
 });
 
-app.mount('#styling')
+app.mount('#user-goals');
