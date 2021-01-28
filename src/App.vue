b6b4f9a9f2923254630372c01459be14847081d6
Ammar Bin Nasir <majidammar428@gmail.com>
2021-01-28 23:21:10 +0500

    Added a component and used Props
    Added a component, imported it in the main.js file. And used app.component('tag-name', <importedComponent>) to be able to use the component.
    
    -- Props --
    
    Props are used for parent child communication.
    
    Parent (App.vue) is used to send data.
    
    Child (FriendContact.vue) accepts the data, using the props keyword.

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,40 @@
 <template>
-
-
-<section>
-  <h2> My friends </h2>
-  <ul>
-    <li></li>
-  </ul>
-</section>
-
-
-  
+  <section>
+    <header><h1>My friends</h1></header>
+    <ul>
+      <friend-contact
+        name="Ammar Nasir"
+        phone-number="0132 1231 123"
+        email-address="some1@some.com"
+      ></friend-contact>
+      <friend-contact
+        name="Some2 Some2"
+        phone-number="0213132 1231 123"
+        email-address="some2@some.com"
+      ></friend-contact>
+    </ul>
+  </section>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        friends: [
-          {
-            id: 'Ammar',
-            number: '11123213',
-            email: 'fictional@gm.com'
-          },
-          {
-            id: 'test',
-            number: 'asd',
-            email: 'haha@gm.com'
-          }
-        ]
-      };
-    }
-  }
+import FriendContact from "./components/FriendContact.vue";
+export default {
+  components: { FriendContact },
+  data() {
+    return {
+      friends: [
+        {
+          id: "Ammar",
+          number: "11123213",
+          email: "fictional@gm.com",
+        },
+        {
+          id: "test",
+          number: "asd",
+          email: "haha@gm.com",
+        },
+      ],
+    };
+  },
+};
 </script>
