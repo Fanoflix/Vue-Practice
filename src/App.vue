891604b1a4679b3dbd558f61c8ac5904f52295aa
Ammar Bin Nasir <majidammar428@gmail.com>
2021-01-28 22:22:22 +0500

    Created a basic vue app.
    Instead of storing the app in a const, we have to export it as default, and import it in the main.js file (the import part is already done)

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,34 @@
 <template>
-  <section>
-    <h2>My Friends</h2>
-    <ul>
-      <li></li>
-    </ul>
-  </section>
+
+
+<section>
+  <h2> My friends </h2>
+  <ul>
+    <li></li>
+  </ul>
+</section>
+
+
+  
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      friends: [
-        {
-          id: "manuel",
-          name: "Manuel Lorenz",
-          phone: "0123 45678 90",
-          email: "manuel@localhost.com",
-        },
-        {
-          id: "julie",
-          name: "Julie Jones",
-          phone: "0987 654421 21",
-          email: "julie@localhost.com",
-        },
-      ],
-    };
-  },
-};
+  export default {
+    data() {
+      return {
+        friends: [
+          {
+            id: 'Ammar',
+            number: '11123213',
+            email: 'fictional@gm.com'
+          },
+          {
+            id: 'test',
+            number: 'asd',
+            email: 'haha@gm.com'
+          }
+        ]
+      };
+    }
+  }
 </script>
