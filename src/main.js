fd3b6d902b7b6dfe191ee40cfa37a2e5aa9973cc
Ammar Bin Nasir <majidammar428@gmail.com>
2021-01-29 01:26:09 +0500

    Making a new Component
    Made a add new friend component that asks the user for data and then the data is emitted to the parent (App.vue) and then displayed.

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import { createApp } from 'vue';
 
 import App from './App.vue';
 import FriendContact from './components/FriendContact.vue';
+import NewFriend from './components/NewFriend.vue'
 
 const app = createApp(App).mount('#app');
 
-app.component('friend-contact', FriendContact);
+app.component('friend-contact', FriendContact); // The tag name should be a two word with a dash in between
+app.component('new-friend', NewFriend); 
 app.mount('#app');
