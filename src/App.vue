ee4f0ddd4732c812d1ae515d743bf6a503a34a51
Ammar Bin Nasir <majidammar428@gmail.com>
2021-01-31 03:39:03 +0500

    Scoped Styling
    Simple concept. Styling only affects the component.

--- src/App.vue
+++ src/App.vue
@@ -12,12 +12,16 @@
 
 <script>
 import TheHeader from './components/TheHeader.vue'; // Registering Locally
+import BadgeList from './components/BadgeList.vue';
+import UserInfo from './components/UserInfo.vue';
 
 export default {
   components: { // Registering Locally
-    // 'the-header': TheHeader, //(1) key is the custom tag that we want, and the value is what we imported.
+    'the-header': TheHeader, //(1) key is the custom tag that we want, and the value is what we imported.
     // TheHeader: TheHeader, //(2) We can use this aswell. And then use TheHeader as the tag in the template (and now you can also use the self closing tags: <TheHeader /> which doesnt work if we use the key value method)
-    TheHeader // (3) This will also work, this basically means (2) but is shorter. In modern Javascript, just a key means "key: key" so here, this basically means TheHeader: TheHeader
+    // TheHeader, // (3) This will also work, this basically means (2) but is shorter. In modern Javascript, just a key means "key: key" so here, this basically means TheHeader: TheHeader
+    BadgeList,
+    UserInfo,
 
   },
   data() {
