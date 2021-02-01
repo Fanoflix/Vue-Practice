17eec4cfb64ee8f17b0d740151af20937ee9bec6
Ammar Bin Nasir <majidammar428@gmail.com>
2021-02-02 02:00:30 +0500

    Made a custom Dialog box using components, instead of using the default browser alert.

--- src/components/ManageGoals.vue
+++ src/components/ManageGoals.vue
@@ -1,5 +1,34 @@
 <template>
     <h2>Manage Goals</h2>
-    <input type="text" />
-    
+    <input type="text" ref="goal"/>
+    <button @click="setGoal">Set Goal</button>
+    <error-alert v-if="inputIsInvalid">
+        <h2>Input is Invalid!</h2>
+        <p>Please provide atleast a few characters...</p>
+        <button @click="confirmError">Ok</button>
+    </error-alert>
 </template>
+
+<script>
+import ErrorAlert from './ErrorAlert.vue';
+
+export default {
+  components: { ErrorAlert },
+    data() {
+        return {
+            inputIsInvalid: false,
+        };
+    },
+    methods: {
+        setGoal() {
+            const enteredValue = this.$refs.goal.value; // these dolalr sign methods are really handy. Pay a visit to the docs
+            if (enteredValue === '') {
+                this.inputIsInvalid = true;
+            }
+        },
+        confirmError() {
+            this.inputIsInvalid = false;
+        }
+    }
+}
+</script>
