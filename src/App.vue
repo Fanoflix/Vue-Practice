e123c24fdaf4141cfe1d960028da8e4ec57e1cf9
Ammar Bin Nasir <majidammar428@gmail.com>
2021-02-09 12:11:31 +0500

    New Setup

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,49 @@
 <template>
-  <learning-survey></learning-survey>
-  <user-experiences></user-experiences>
+  <the-navigation @set-page="setActivePage"></the-navigation>
+  <main>
+    <component :is="activePage"></component>
+  </main>
 </template>
 
 <script>
-import LearningSurvey from './components/survey/LearningSurvey.vue';
-import UserExperiences from './components/survey/UserExperiences.vue';
+import TeamsList from './components/teams/TeamsList.vue';
+import UsersList from './components/users/UsersList.vue';
+import TheNavigation from './components/nav/TheNavigation.vue';
 
 export default {
   components: {
-    LearningSurvey,
-    UserExperiences,
+    TheNavigation,
+    TeamsList,
+    UsersList,
   },
-  // data() { // (2) WE NO LONGER NEED TO MANAGE DATA IN OUR VUE APP HERE (YOU COULD HAVE A SCENARIO WHERE YOU NEED TO MANAGE DATA IN THE VUE APP ITSELF, BUT ITS NOT THE CASE HERE)
-  //   return {
-  //     savedSurveyResults: [],
-  //   };
-  // },
-
-  // (3) NO LONGER DOING THIS LOCALLY ANY MORE (FOR THIS PARTICULAR APP)
-  // methods: {
-  //   storeSurvey(surveyData) {
-  //     const surveyResult = {
-  //       name: surveyData.userName,
-  //       rating: surveyData.rating,
-  //       id: new Date().toISOString(),
-  //     };
-  //     this.savedSurveyResults.push(surveyResult);
-  //     console.log(surveyResult);
-  //   },
-  // },
+  data() {
+    return {
+      activePage: 'teams-list',
+      teams: [
+        { id: 't1', name: 'Frontend Engineers', members: ['u1', 'u2'] },
+        { id: 't2', name: 'Backend Engineers', members: ['u1', 'u2', 'u3'] },
+        { id: 't3', name: 'Client Consulting', members: ['u4', 'u5'] },
+      ],
+      users: [
+        { id: 'u1', fullName: 'Max Schwarz', role: 'Engineer' },
+        { id: 'u2', fullName: 'Praveen Kumar', role: 'Engineer' },
+        { id: 'u3', fullName: 'Julie Jones', role: 'Engineer' },
+        { id: 'u4', fullName: 'Alex Blackfield', role: 'Consultant' },
+        { id: 'u5', fullName: 'Marie Smith', role: 'Consultant' },
+      ],
+    };
+  },
+  provide() {
+    return {
+      teams: this.teams,
+      users: this.users,
+    };
+  },
+  methods: {
+    setActivePage(page) {
+      this.activePage = page;
+    },
+  },
 };
 </script>
 
