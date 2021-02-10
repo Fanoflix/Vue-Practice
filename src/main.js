99f33463bce8e2f7de7cdb26076d8f765da32d04
Ammar Bin Nasir <majidammar428@gmail.com>
2021-02-10 20:39:20 +0500

    Another special property from vue-router: this.$route (different from this.$router)
    Another special property which is available. The this.$route (different from this.$router) This is available to this component, because this component is loaded through the router (in main.js)
    
    $route has a the params property, which: holds all the parameters that were used to get to this page. And we currently only have 1 param that was used, which was the 'teamID' (from main.js) We'll use the same name as the one provided in the main.js router.

--- src/main.js
+++ src/main.js
@@ -4,12 +4,15 @@ import App from './App.vue';
 
 import TeamsList from './components/teams/TeamsList.vue';
 import UsersList from './components/users/UsersList.vue';
+import TeamMembers from './components/teams/TeamMembers.vue';
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
+    { path: '/users', component: UsersList },
     { path: '/teams', component: TeamsList },
-    { path: '/users', component: UsersList }
+    // { path: '/teams/new' }, // if we had another /teams/something path, then we'll have to put it above the dynamic path, so.. above the route below.
+    { path: '/teams/:teamID', component: TeamMembers} // We can make dynamic routes, by using the colon : to set  
   ],
   // We can provide additional configuration to our createRouter relating to the classes that are added to the active <router-link> element. These are the linkActiveClass and linkExactActiveClass as the key, and the value should be a string which you instead need to use as the CSS class name.
   linkActiveClass: 'active',
