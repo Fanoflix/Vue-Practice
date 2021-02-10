7df8678095e0c0d5dd0871f1d920e4f88194993f
Ammar Bin Nasir <majidammar428@gmail.com>
2021-02-10 20:51:26 +0500

    Removed Old Comments

--- src/components/teams/TeamMembers.vue
+++ src/components/teams/TeamMembers.vue
@@ -16,7 +16,7 @@
 import UserItem from '../users/UserItem.vue';
 
 export default {
-  inject: ['users', 'teams'], // Injecting here. (These are provided in App.vue) we're using these below in the created() hook at line 28.
+  inject: ['users', 'teams'],
   components: {
     UserItem
   },
@@ -27,15 +27,12 @@ export default {
     };
   },
   created() {
-    // Another special property which is available. The this.$route (different from this.$router) This is available to this component, because this component is loaded through the router (in main.js)
     console.log(this.$$route);
-    const teamID = this.$route.params.teamID // #route has a the params property, which: holds all the parameters that were used to get to this page. And we currently only have 1 param that was used, which was the 'teamID' (from main.js) We'll use the same name as the one provided in the main.js router.
-
-    // Below is the logic for extracting the team from the teams array, by using the ID provided through the URL. Then making an empty array, and then extracting all the users from the selected team one by one and pushing each user to this empty array.
+    const teamID = this.$route.params.teamID
      
-    // Note: each team, just has a bunch of userIDs:
+    // Note: each team, just has a bunch of userIDs
     // { id: 't1', name: 'Frontend Engineers', members: ['u1', 'u2'] },
-    const selectedTeam = this.teams.find(team=> team.id === teamID); // find is a JS function that iterates through each element. So here, its iterating on the teams array, and for each team in teams we're checking if the team.id === teamID, and it then returns that element if the expression is true.
+    const selectedTeam = this.teams.find(team => team.id === teamID);
     const members = selectedTeam.members;
     const selectedMembers = [];
     for (const member of members) {
