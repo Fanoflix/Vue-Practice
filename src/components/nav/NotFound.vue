1b4d4cad1b70d0bfb667394202c1b360b0e7f05b
Ammar Bin Nasir <majidammar428@gmail.com>
2021-02-11 20:26:18 +0500

    Handling all routes that we haven't defined
    So we add a new route at the end (because it should have the lowest priority, and should only kick in if no other route handles it) and give it a path of '/:notFound(.*)'
    So:
    
        { path: '/:notFound(.*)', redirect: '/teams'}
    
    .* is a regex (Regular Expression) which handles any character combination (this is basically why we wanted it at the end.
    
    We can redirect, or be more creative, and create a pageNotFound Component and load it here.
    
        { path: '/:notFound(.*)', component: 'NotFound' }

---
+++ src/components/nav/NotFound.vue
@@ -0,0 +1,5 @@
+<template>
+    <h2>Page not found! Maybe you were looking for our
+        <router-link to="/teams"> Teams Page </router-link> ?
+    </h2>
+</template>
--- src/main.js
+++ src/main.js
@@ -5,15 +5,17 @@ import App from './App.vue';
 import TeamsList from './components/teams/TeamsList.vue';
 import UsersList from './components/users/UsersList.vue';
 import TeamMembers from './components/teams/TeamMembers.vue';
+import NotFound from './components/nav/NotFound.vue';
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    // {path: '/', redirect: '/teams'},
+    { path: '/', redirect: '/teams' },
     { path: '/users', component: UsersList },
-    { path: '/teams', component: TeamsList, alias: '/' },
+    { path: '/teams', component: TeamsList },
     // { path: '/teams/new' },
-    { path: '/teams/:teamID', component: TeamMembers, props:true} // props: true tells the vue-router, that the dynamic param should be passed into this component as props, rather than just on the $route property. So now: teamID here, is the prop. And you can just use the props: [] property in the script tag of the component itself. So this is always the better way.
+    { path: '/teams/:teamID', component: TeamMembers, props:true},
+    { path: '/:notFound(.*)', component: NotFound }
   ],
   linkActiveClass: 'active',
   linkExactActiveClass: 'exactActive',
