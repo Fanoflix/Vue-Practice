7583c0ebba6c6f2a387dcdfb359fe731ae313915
Ammar Bin Nasir <majidammar428@gmail.com>
2021-02-13 20:21:21 +0500

    Controlling scroll with scrollBehaviour in main.js
    scrollBehavior is a method, which will be called, whenever our page changes. It receives 3 arguments, to from, savedPosition.
    
    This method is added to main.js file as one of the routes config.

--- src/main.js
+++ src/main.js
@@ -13,24 +13,34 @@ const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', redirect: '/teams' },
-    { path: '/users', components: {default: UsersList, footer: UsersFooter} }, 
+    { path: '/users', components: { default: UsersList, footer: UsersFooter } },
     {
       name: 'teams',
       path: '/teams',
-      components:{ default: TeamsList, footer: TeamsFooter }, // keys identify different router views, and the values are the components that should be loaded. "default" corresponds to unnamed router-view
+      components: { default: TeamsList, footer: TeamsFooter },
       children: [
         {
           name: 'team-members',
           path: ':teamID',
-          component: TeamMembers, 
+          component: TeamMembers,
           props: true
         }
       ]
     },
-    { path: '/:notFound(.*)', component: NotFound } // Any route with just 'component' (without the 's') will be caught by the unname route. So if we enter an invalid URL, we'll only be shown the 'NotFound' Component.
+    { path: '/:notFound(.*)', component: NotFound }
   ],
   linkActiveClass: 'active',
-  linkExactActiveClass: 'exactActive'
+  linkExactActiveClass: 'exactActive',
+  scrollBehavior(to, from, savedPosition) {
+    // Scrollbehavioru is a method, which will be called, whenever our page changes. It receives 3 arguments, to from, savedPosition
+    console.log(to, from, savedPosition); // savedPosition is the scroll position we had on the page before we went backward.
+
+    if (savedPosition) { // (2) If savePosition exists we go to that position. If savePosition is null, we jump to the top. So basically, whenever we click "back" we'll always be returned to our last savedPosition instead of aalways ending at the top (which was the behaviour when we only added (1) )
+      return savedPosition;
+    }
+
+    return { left:0, top: 0} // (1) If we always return this, we will always scrolled 0 px from the top (so at the top) whenever the page changes
+  }
 });
 
 const app = createApp(App);
