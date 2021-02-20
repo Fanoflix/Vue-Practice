5e6d6163454997a5259357cde2937f4f6e18ad74
Ammar Bin Nasir <majidammar428@gmail.com>
2021-02-21 01:55:52 +0500

    Setup Vuex

--- src/main.js
+++ src/main.js
@@ -1,7 +1,18 @@
 import { createApp } from 'vue';
+import { createStore } from 'vuex';
 
 import App from './App.vue';
 
-const app = createApp(App);
+const store = createStore({
+  // Create Store takes an object where we configure the store.
+  state() {
+    // state is a method, which returns the state object.
+    return {
+      counter: 0
+    };
+  }
+});
+const app = createApp(App); 
 
+app.use(store); // Using the store
 app.mount('#app');
